372784ef9f969aa78b1b174b0fc152b28339dbf8
Fazza Bajwa <fazza.bajwa@kcl.ac.uk>
2022-10-07 14:30:10 +0100

    Solution

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def checkIfPrime(number):
+    isPrime = True
+    for i in range (2,number):
+        if number%i == 0:
+            isPrime = False
+    return isPrime
+
 def primes(number_of_primes):
     list = []
+    if number_of_primes > 0:
+        num = 1
+        while len(list) < number_of_primes:
+            num = num + 1
+            if checkIfPrime(num) == True:
+                list.append(num)
+    else:
+        raise ValueError("The number of primes cannot be less than 0.")
     return list
